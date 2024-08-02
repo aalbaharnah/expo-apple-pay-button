5de903f385528b5700e7ad0072efa1a7777bfff1
Mustafa Alsihati <51824138+MustafaAlsihati@users.noreply.github.com>
2024-08-02 15:07:50 +0300

    Added web integration and apple pay button for web

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import { PaymentOptions } from './ExpoApplePayButton.types';
 import ExpoApplePayButtonModule from './ExpoApplePayButtonModule';
 
-
 export {
   default as ApplePayButton,
 } from './ExpoApplePayButtonView';
 
 export * from './ExpoApplePayButton.types';
 
+/**
+ * @description This function initializes the Apple Pay session, it's required for only for web.
+ */
+export function initApplePaySession(data: ApplePayJS.ApplePayPaymentRequest): ApplePaySession | null {
+  return null;
+}
+
+/**
+ * @description This function starts the Apple Pay payment, in web it is required to pass the ApplePaySession and onValidateMerchant callback.
+ */
 export async function startPaymentAsync(options: PaymentOptions) {
   return ExpoApplePayButtonModule.startPaymentAsync(options);
 }
@@ -15,3 +24,8 @@ export async function startPaymentAsync(options: PaymentOptions) {
 export async function canMakePaymentsAsync(): Promise<boolean> {
   return ExpoApplePayButtonModule.canMakePaymentsAsync();
 }
+
+/**
+ * @description This function ends Apple Pay session, it's required for only for web.
+ */
+export async function completePaymentSession(applePaySession?: ApplePaySession | null, error = false) {}
